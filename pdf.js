21c4ca15a5b941f8dd98780d04bbf2a4ab907f2f
kirylvarykau <cyril.varikov@gmail.com>
2024-07-07 12:26:15 +0200

    updated CV with latest changes

--- pdf.js
+++ pdf.js
@@ -1,26 +1,26 @@
 import puppeteer from "puppeteer";
-import fs from 'fs';
-import {setTimeout} from 'timers/promises'
-
+import fs from "fs";
+import { setTimeout } from "timers/promises";
 
 (async () => {
-  if(fs.existsSync('result.pdf')) {
-    fs.rmSync('result.pdf');
+  if (fs.existsSync("result.pdf")) {
+    fs.rmSync("result.pdf");
   }
-  const browser = await puppeteer.launch({headless: true});
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto('http://localhost:3000/', { waitUntil: 'load' });
+  await page.goto("http://localhost:3000/", { waitUntil: "load" });
   await setTimeout(5000);
 
   // await page.emulateMediaType('screen');
 
   const pdf = await page.pdf({
-    path: 'result.pdf',
-    margin: { top: '50px', right: '20px', bottom: '50px', left: '20px' },
+    path: "result.pdf",
+    margin: { top: "50px", right: "20px", bottom: "50px", left: "20px" },
     printBackground: true,
     preferCSSPageSize: true,
-    format: 'A2',
+    format: "A4",
   });
 
   await browser.close();
-})()
+})();
+
